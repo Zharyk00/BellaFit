cb0f9971876c028a50be2e8c659f8ceb9f96d590
Zharyk <aijoka99@gmail.com>
2023-01-13 18:40:32 +0600

    add image for about page and change text

--- src/Components/AboutUs.js
+++ src/Components/AboutUs.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import '../Styles/About/About.css'
 
 function AboutUs() {
     return (
-        <div>AboutUs</div>
+        <main className='main_about_content'>
+            <div className='first_block'>
+                <p className='back_text'>About Us</p>
+                <div className='back_image'></div>
+            </div>
+        </main>
     )
 }
 
